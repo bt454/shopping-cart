b351c3143d3a49ba9ea19c95be8135f50843a413
Ben Tipper <ben.tipper@meliore.co.uk>
2020-04-07 13:19:06 +0100

    Failing second test for ShopProduce.fromString().

--- src/test/scala/com/meliore/shopping/ShopProduceSpec.scala
+++ src/test/scala/com/meliore/shopping/ShopProduceSpec.scala
@@ -1,6 +1,6 @@
 package com.meliore.shopping
 
-import com.meliore.shopping.ShopProduce.{Apple, fromString}
+import com.meliore.shopping.ShopProduce.{Apple, Orange, fromString}
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 
@@ -10,6 +10,12 @@ class ShopProduceSpec extends AnyWordSpec with Matchers {
     "supplied with the name Apple" should {
       "result in a new object of type Apple" in {
         fromString("Apple") shouldBe Apple()
+      }
+    }
+
+    "supplied with the name Orange" should {
+      "result in a new object of type Orange" in {
+        fromString("Orange") shouldBe Orange()
       }
     }
   }
