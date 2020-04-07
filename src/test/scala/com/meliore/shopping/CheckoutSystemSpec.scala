26f3e869101ee50c44be1cc9b8dfb0a7ff78e392
Ben Tipper <ben.tipper@meliore.co.uk>
2020-04-07 13:10:27 +0100

    Added failing test for CheckoutSystem.

---
+++ src/test/scala/com/meliore/shopping/CheckoutSystemSpec.scala
@@ -0,0 +1,26 @@
+package com.meliore.shopping
+
+import org.mockito.MockitoSugar
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+class CheckoutSystemSpec extends AnyWordSpec with Matchers with MockitoSugar {
+
+  private val checkoutCalculator = mock[CheckoutCalculator]
+  private val costFormatter = mock[CostFormatter]
+  private val checkoutSystem = new CheckoutSystem(checkoutCalculator, costFormatter)
+
+  "checkout" when {
+    "supplied with a list of fruits" should {
+      "result in the total cost as a string with currency" in {
+        val fruits = List(Apple(), Orange(), Apple(), Apple(), Orange(), Apple(), Orange(), Orange(), Apple())
+        val total = 3.40
+
+        when(checkoutCalculator.calculateTotal(fruits)).thenReturn(total)
+        when(costFormatter.format(total)).thenReturn("£3.40")
+
+        checkoutSystem.checkout(fruits) shouldBe "£3.40"
+      }
+    }
+  }
+}
